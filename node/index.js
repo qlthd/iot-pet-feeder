5a8576a4a87ded6e782343f72af0cbf5f10a3ee3
Unknown <quentinlathaud@hotmail.fr>
2019-12-15 17:39:50 +0100

    Communications OK

--- node/index.js
+++ node/index.js
@@ -30,69 +30,96 @@ app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
   res.render('home.ejs');
   console.log('Accueil demandé')
-});
+})
 
 app.get('/portions', function (req, res) {
   res.render('portionselector.ejs');
-});
+})
 
 app.get('/feedscheduler', function (req, res) {
-	const schedules = [ {id : 1, day : "15/01/2019", hour : "09:34", portions : "7"} ,
-		{ id : 2, day : "15/10/2019", hour : "12:31", portions : "2"}]
-
 	var mqtt = require('mqtt');
-	var client  = mqtt.connect('mqtt://192.168.8.108')
+	var client  = mqtt.connect('mqtt://192.168.8.107')
 	client.on('connect', function () {
-		client.subscribe('nbPortions')
+		client.publish('schedules/get','');
 	})
+
+	client.subscribe('schedules/send')
+
 	client.on('message', function (topic, message) {
-		if(topic == 'schedules/get'){
-			context = message.toString();
-			console.log(context)
+		if(topic == "schedules/send"){
+			const schedules = JSON.parse(message.toString())
+			res.render('feedscheduler.ejs',{schedules : schedules});
+
 		}
 	})
-
-
-
-	res.render('feedscheduler.ejs',{schedules : schedules});
-});
+})
 
 app.get('/movesensorslog', function (req, res) {
-	const movements = [ {day : "15/01/2019", hour : "09:34"} ,
-		{day : "15/10/2019", hour : "12:31"}]
-	res.render('movesensorslog.ejs',{ movements : movements});
-});
-
-
-app.post('/nbPortions', function(req, res) {
-	var nbPortions = req.body.ps
 	var mqtt = require('mqtt');
-	var client  = mqtt.connect('mqtt://192.168.8.108');
-
-
-
+	var client  = mqtt.connect('mqtt://192.168.8.107')
 	client.on('connect', function () {
-		client.publish('nbPortions',nbPortions);
-		console.log('Message Sent');
-	
+		client.publish('sensor/get','');
+	})
 
-	});
+	client.subscribe('sensor/send')
+
+	client.on('message', function (topic, message) {
+		if(topic == "sensor/send"){
+			const movements = JSON.parse(message.toString())
+			res.render('movesensorslog.ejs',{ movements : movements});
+		}
+	})
 
 })
 
 
+app.post('/feed', function(req, res) {
+	var nbPortions = req.body.ps
+	var mqtt = require('mqtt')
+	var client  = mqtt.connect('mqtt://192.168.8.107')
+	client.on('connect', function () {
+		client.publish('feed',nbPortions)
+		console.log(nbPortions + ' portions sent !')
+	});
+	res.end();
+})
+
+
 app.post('/delete_schedule', function(req, res) {
-	var id= req.body.id;
+	var id= req.body.button;
+	console.log("id :"+id)
 	var mqtt = require('mqtt');
-	var client  = mqtt.connect('mqtt://192.168.8.108');
+	var client  = mqtt.connect('mqtt://192.168.8.107');
 
 	client.on('connect', function () {
-		client.publish('schedules/delete',id);
+		client.publish('schedules/delete',id.toString());
 		console.log('schedules/delete Sent');
-
-
 	});
+	res.redirect('/feedscheduler')
 
+
+})
+
+app.post('/add_schedule', function(req, res) {
+	var date= new Date(req.body.date);
+	var day = date.getDate().toString().length > 1 ? date.getDate() : "0"+date.getDate().toString()
+	var month = (date.getMonth()+1).toString().length > 1 ? (date.getMonth()+1) : "0"+(date.getMonth()+1)
+	var hours = date.getHours().toString().length > 1 ? date.getHours() : "0"+date.getHours().toString()
+
+	var minutes = date.getMinutes().toString().length > 1 ? date.getMinutes() : "0"+date.getMinutes().toString()
+
+	var date2 = day+"/"+month+"/"+date.getFullYear()+" "+hours+":"+minutes
+
+	console.log(date2)
+	var portions = req.body.ps;
+
+	var mqtt = require('mqtt');
+	var client  = mqtt.connect('mqtt://192.168.8.107');
+	var message = date2 + " "+ portions
+	client.on('connect', function () {
+		client.publish('schedules/add',message);
+	});
+	res.redirect('/feedscheduler')
 })
 
 
--- python/script.py
+++ python/script.py
@@ -1,28 +1,103 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
+import datetime
+import json
 
 def turn_turbine(nb):
 	for i in range(1,int(nb)) :
 		print("Tourne !")
+
+def on_connect(client, userdata, flags, rc):
+     print("Connected flags"+str(flags)+"result code "\
+     +str(rc)+"client1_id ")
+     client.connected_flag=True
+
+def on_publish(client,userdata,result):
+    print("data published \n")
+
+def write_feed_schedule(line):
+    file = open("schedules.txt", "a+")
+    file.write(line+"\r\n")
+
+def write_log_sensor():
+    f = open("sensor_log.txt","a+")
+    d = datetime.datetime.today()
+    f.write(str(d)+"\n")
+    f.close()
+
+def read_feed_schedule():
+    fileHandler = open ("schedules.txt", "r")
+    list = []
+    i=0
+    while True:
+        line = fileHandler.readline()
+        if not line :
+            break
+        day,hour,portion=line.split()
+        list.append({ "id": i, "day" : day,"hour" : hour,"portions" : portion })
+        i= i+1
+    return json.dumps(list)
+
+def read_sensor_log():
+    fileHandler = open ("sensor_log.txt", "r")
+    list = []
+    i=0
+    while True:
+        line = fileHandler.readline()
+        if not line :
+            break
+        date,hour=line.split()
+        list.append({ "id": i, "date" : date,"hour" : hour})
+        i= i+1
+    return json.dumps(list)
+
+
+def delete_schedule(i):
+    with open("schedules.txt", "r") as infile:
+        lines = infile.readlines()
+
+    with open("schedules.txt", "w") as outfile:
+        for pos, line in enumerate(lines):
+            if pos != i:
+                outfile.write(line)
 
 
 ############
 def on_message(client, userdata, message):
     print("message received " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
-    turn_turbine(str(message.payload.decode("utf-8")))
+    if (message.topic == "feed"):
+        turn_turbine(str(message.payload.decode("utf-8")))
+    elif (message.topic == "schedules/add"):
+        write_feed_schedule(str(message.payload.decode("utf-8")))
+    elif (message.topic == "schedules/get"):
+        list = read_feed_schedule()
+        client.publish("schedules/send",list)
+    elif (message.topic == "schedules/delete"):
+        i = str(message.payload.decode("utf-8"))
+        delete_schedule(int(i))
+    elif (message.topic == "sensor/get"):
+        list = read_sensor_log()
+        client.publish("sensor/send",list)
 ########################################
 
 
 
-broker_address="192.168.8.108" 
-client = mqtt.Client("P1") #create new instance
-client.connect(broker_address) #connect to broker
-client.loop_start()
-client.on_message=on_message
-client.subscribe("nbPortions")
-time.sleep(20)
-client.loop_stop()
 
-
+def main():
+    broker_address="192.168.8.107"
+    global client
+    client = mqtt.Client("rasp") #create new instance
+    client.on_connect = on_connect
+    client.connect(broker_address) #connect to broker
+    client.on_message=on_message
+    client.on_publish=on_publish
+    client.subscribe("feed")
+    client.subscribe("schedules/add")
+    client.subscribe("schedules/get")
+    client.subscribe("schedules/delete")
+    client.subscribe("sensor/get")
+    client.loop_forever()
+if __name__ == "__main__":
+    main()
 
